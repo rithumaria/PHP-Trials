d59a75bcbba9d70f04ab8e06c50c74a8ef3463fd
RITHU MARIA JOSE <37535041+rithumaria@users.noreply.github.com>
2019-08-18 00:55:46 +0530

    First Commit
    Mad Libs are short stories with blank spaces, which get filled in by the user. The result is usually funny (or strange).
    
    Mad Libs requires:
    
    A short story with blank spaces (asking for different types of words).
    Words to fill in those blanks.

---
+++ Mad_Lib_Function.php
@@ -0,0 +1,12 @@
+<?php
+function generateStory($singular_noun, $verb, $color, $distance_unit)
+{
+  $story = "\nThe $singular_noun are lovely, $color, and deep.
+\nBut I have promises to keep,
+\nAnd $distance_unit to go before I $verb,
+\nAnd $distance_unit to go before I $verb.\n";
+  return $story;
+}
+echo generateStory("dog", "eat", "purple","4");
+echo generateStory("car", "cook", "vermilion", "3");
+echo generateStory("empty void", "speak", "beige", "9");
