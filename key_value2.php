3543ca1d5b40e94d051b2f191d33841e18e3b84b
RITHU MARIA JOSE <37535041+rithumaria@users.noreply.github.com>
2019-08-19 22:35:00 +0530

    first commit

---
+++ key_value2.php
@@ -0,0 +1,20 @@
+<?php
+namespace Codecademy;
+
+$my_car = [
+  "oil" => "black and clumpy",
+  "brakes" => "new",
+  "tires" => "old with worn treads",
+  "filth" => "bird droppings", 
+  "wiper fluid" => "full", 
+  "headlights" => "bright"
+];
+print_r($my_car);
+
+// Write your code below:
+unset($my_car["oil"]);
+$my_car["oil"]="new and premium";
+unset($my_car["tires"]);
+$my_car["tires"]="new with deep treads";
+unset($my_car["filth"]);
+print_r($my_car);
